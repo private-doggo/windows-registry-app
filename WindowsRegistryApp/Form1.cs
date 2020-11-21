8b80b05dcfd879e202bdc1720c293b3f1d91b99e
private-doggo <github.penguin.anonaddy.com>
2020-11-21 23:39:07 +0300

    added registry root keys to the list and their sub keys

--- WindowsRegistryApp/Form1.cs
+++ WindowsRegistryApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -26,7 +27,69 @@ namespace WindowsRegistryApp
         {
             // filling treeView with keys
 
-            
+            treeView_registryKeys.Nodes.Add(new TreeNode("Computer"));
+
+            RegistryKey[] regKeyArray = new RegistryKey[] { Registry.ClassesRoot,
+                                                            Registry.CurrentUser,
+                                                            Registry.LocalMachine,
+                                                            Registry.Users,
+                                                            Registry.CurrentConfig,
+                                                            // Registry.PerformanceData
+															// Registry.DynData
+			                                                };
+
+            foreach (RegistryKey key in regKeyArray)
+            {
+                TreeNode node = new TreeNode() { Name = "node", Text = key.ToString() };
+                treeView.FindNodeByName(treeView_registryKeys.Nodes[0], "Computer").Nodes.Add(node);
+            }
+
+            treeView.FindNodeByName(treeView_registryKeys.Nodes[0], "Computer").Expand();
+
+            String[] regClassesRootKeyArray = new String[1000];
+            regClassesRootKeyArray = Registry.ClassesRoot.GetSubKeyNames();
+
+            foreach (String subKey in regClassesRootKeyArray)
+            {
+                TreeNode node = new TreeNode() { Name = "node", Text = subKey };
+                treeView.FindNodeByName(treeView_registryKeys.Nodes[0], "HKEY_CLASSES_ROOT").Nodes.Add(node);
+            }
+
+            String[] regCurrentUserKeyArray = new String[1000];
+            regCurrentUserKeyArray = Registry.CurrentUser.GetSubKeyNames();
+
+            foreach (String subKey in regCurrentUserKeyArray)
+            {
+                TreeNode node = new TreeNode() { Name = "node", Text = subKey };
+                treeView.FindNodeByName(treeView_registryKeys.Nodes[0], "HKEY_CURRENT_USER").Nodes.Add(node);
+            }
+
+            String[] regLocalMachineKeyArray = new String[1000];
+            regLocalMachineKeyArray = Registry.LocalMachine.GetSubKeyNames();
+
+            foreach (String subKey in regLocalMachineKeyArray)
+            {
+                TreeNode node = new TreeNode() { Name = "node", Text = subKey };
+                treeView.FindNodeByName(treeView_registryKeys.Nodes[0], "HKEY_LOCAL_MACHINE").Nodes.Add(node);
+            }
+
+            String[] regUsersKeyArray = new String[1000];
+            regUsersKeyArray = Registry.Users.GetSubKeyNames();
+
+            foreach (String subKey in regUsersKeyArray)
+            {
+                TreeNode node = new TreeNode() { Name = "node", Text = subKey };
+                treeView.FindNodeByName(treeView_registryKeys.Nodes[0], "HKEY_USERS").Nodes.Add(node);
+            }
+
+            String[] regCurrentConfigKeyArray = new String[1000];
+            regCurrentConfigKeyArray = Registry.CurrentConfig.GetSubKeyNames();
+
+            foreach (String subKey in regCurrentConfigKeyArray)
+            {
+                TreeNode node = new TreeNode() { Name = "node", Text = subKey };
+                treeView.FindNodeByName(treeView_registryKeys.Nodes[0], "HKEY_CURRENT_CONFIG").Nodes.Add(node);
+            }
         }
     }
 }
---
+++ WindowsRegistryApp/treeView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsRegistryApp
+{
+    class treeView
+    {
+        static public TreeNode FindNodeByName(TreeNode root, string name)
+        {
+            if (root == null)
+                return null;
+
+            if (root.Text == name)
+                return root;
+            return FindNodeByName(root.FirstNode, name) ?? FindNodeByName(root.NextNode, name);
+        }
+    }
+}
